b37ba8f3c3331c510cbaeca02e9a59af8fdb2f71
mariadoan247 <mariadoan247@ou.edu>
2023-04-27 13:09:22 -0500

    Initialized with base code

---
+++ Problem_2/UDPServer_multiply.py
@@ -0,0 +1,17 @@
+from socket import *
+# Create a UDP socket
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+# Bind the socket to port 12000
+serverPort = 12000
+serverSocket.bind(('',serverPort))
+print('The server is ready to receive')
+while True:
+    # Receive a byte string up to 2048 bytes long from the socket
+    # The sender address is attached with the message
+    message, clientAddress = serverSocket.recvfrom(2048)
+    clientIP, clientPort = clientAddress
+    replyMessage = input('Type a message to reply:')
+    print(f'message from {clientIP}:{clientPort} = {message}')
+    # Send a message through the socket
+    # The destination address is provided
+    serverSocket.sendto(replyMessage.encode(), clientAddress)
